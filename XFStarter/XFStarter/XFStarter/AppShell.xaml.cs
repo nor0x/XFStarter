6f98b51b8ffb97e397b29fa7584e9aff91c96e8d
Johnny <nor0x@users.noreply.github.com>
2020-10-27 14:20:36 +0100

    first commit

---
+++ XFStarter/XFStarter/XFStarter/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("tabler-icons.ttf", Alias = "TablerIcons")]
+
+namespace XFStarter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XFStarter/XFStarter/XFStarter/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TinyMvvm;
+
+namespace XFStarter.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
